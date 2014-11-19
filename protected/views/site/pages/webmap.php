c256465e53789f912c3b39ee9a067b723f1fdef9
nyansilo <danielgeuza808@gmail.com>
2014-11-19 13:33:16 +0300

    create link for lindimaphp & rukwamap.php

--- protected/views/site/pages/webmap.php
+++ protected/views/site/pages/webmap.php
@@ -20,8 +20,8 @@ $this->breadcrumbs=array(
 	             .'<th>'
 	
 			
-						   . TbHtml::linkbutton('01.Arusha ', array( 'url' => './uploads/arusha.php','color' =>TbHtml::BUTTON_COLOR_PRIMARY,)).'<br>'
-						   . TbHtml::linkbutton('02.Dar es salaam  ', array( 'url' => './uploads/dar_es_salaam.php','color' =>TbHtml::BUTTON_COLOR_WARNING, )).'<br>'
+						   . TbHtml::linkbutton('01.Arusha ', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_PRIMARY,)).'<br>'
+						   . TbHtml::linkbutton('02.Dar es salaam  ', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_WARNING, )).'<br>'
 						   . TbHtml::linkbutton('03.Dodoma ', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_PRIMARY, )).'<br>'
 						   . TbHtml::linkbutton('04.Iringa ', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_PRIMARY, )).'<br>'
 						   . TbHtml::linkbutton('05.Kagera ', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_PRIMARY, )).'<br>'
@@ -33,12 +33,12 @@ $this->breadcrumbs=array(
 				 
 				 '<th>'.
 			
-						     TbHtml::linkbutton('08.Lindi', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_SUCCESS, )).'<br>'
+						     TbHtml::linkbutton('08.Lindi', array( 'url' => './uploads/lindimap.php','color' =>TbHtml::BUTTON_COLOR_SUCCESS, 'target'=>'_blank' )).'<br>'
 						   . TbHtml::linkbutton('09.Manyara', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_SUCCESS, )).'<br>'
 						   . TbHtml::linkbutton('10.Mara', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_SUCCESS, )).'<br>'
 						   . TbHtml::linkbutton('11.Mbeya', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_SUCCESS, )).'<br>'
 						   . TbHtml::linkbutton('12.Morogoro', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_SUCCESS, )).'<br>'
-						   . TbHtml::linkbutton('13.Mtwara', array( 'url' => './uploads/mtwara.php','color' =>TbHtml::BUTTON_COLOR_SUCCESS, 'target'=>'_blank' )).'<br>'
+						   . TbHtml::linkbutton('13.Mtwara', array( 'url' => './uploads/mtwaramap.php','color' =>TbHtml::BUTTON_COLOR_SUCCESS, 'target'=>'_blank' )).'<br>'
 						   . TbHtml::linkbutton('14.Mwanza', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_SUCCESS, )).
 						   
 				 '<th>'.
@@ -47,7 +47,7 @@ $this->breadcrumbs=array(
 				 '<th>'.
 
 						     TbHtml::linkbutton('15.Pwani', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_DANGER, )).'<br>'
-						   . TbHtml::linkbutton('16.Rukwa', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_DANGER, )).'<br>'
+						   . TbHtml::linkbutton('16.Rukwa', array( 'url' => './uploads/rukwamap.php','color' =>TbHtml::BUTTON_COLOR_DANGER, 'target'=>'_blank' )).'<br>'
 						   . TbHtml::linkbutton('17.Ruvuma', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_DANGER, )).'<br>'
 						   . TbHtml::linkbutton('18.Shinyanga', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_DANGER, )).'<br>'
 						   . TbHtml::linkbutton('19.Singida', array( 'url' => '#','color' =>TbHtml::BUTTON_COLOR_DANGER, )).'<br>'
